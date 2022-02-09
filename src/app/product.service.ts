50da1d70981388b459e30ff0961f5bf8cb8bf54d
mjohnston-rightnow <matthewjohnston@rightnow.org>
2022-02-09 11:19:59 -0600

    AlbumStore casted the way Pluralsight wanted me to

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators/map';
 import { Album } from './album';
 
 @Injectable()
@@ -13,6 +13,6 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http
       .get(this._albumUrl)
-      .pipe(map((response: Response) => response.json() as Album));
+      .pipe(map((response: Response) => <Album>response.json()));
   }
 }
