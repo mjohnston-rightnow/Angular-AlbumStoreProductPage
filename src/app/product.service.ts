4cdf7700558ab5d966cc62730e26d9eec0b13bcb
mjohnston-rightnow <matthewjohnston@rightnow.org>
2022-02-09 11:00:40 -0600

    AlbumStore wrote deperecated code to satisfy Pluralsight

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -11,6 +11,6 @@ export class ProductService {
   getAlbum(id: number) {
     return this._http
       .get(this._albumUrl)
-      .pipe(map((response) => response.json()));
+      .pipe(map((response: Response) => response.json()));
   }
 }
