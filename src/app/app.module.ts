5e832b82bbb2df3435ff12fa5a913ca852c65fb8
mjohnston-rightnow <matthewjohnston@rightnow.org>
2022-02-09 12:42:57 -0600

    AlbumStore correctly added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -10,7 +10,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductService } from './product.service';
 
-const appRoutes = [
+const appRoutes: Routes = [
   {
     path: 'products',
     component: ProductListComponent,
